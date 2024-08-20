b8137c56220184635b77b77d64147b8265288931
Rink <bystrova1979inbox.ru@gmail.com>
2024-08-20 18:35:13 +0300

    Change the logic of above and below objects, stuck at the resetting rigidbody forces

--- prototypesym/character.gd
+++ prototypesym/character.gd
@@ -22,7 +22,7 @@ func _process(_delta):
 		velocity.y += gravity
 		if velocity.y > 1000:
 			velocity.y = 1000
-	is_above = global_position.y < axis.global_position.y
+	check_is_above()
 	
 	if Input.is_action_just_pressed('w') and is_on_floor():
 		velocity.y = -500
@@ -30,3 +30,6 @@ func _process(_delta):
 	velocity.x = 100 * horz
 	velocity.x = 300 * horz
 	move_and_slide()
+
+func check_is_above():
+	is_above = global_position.y < axis.global_position.y
--- prototypesym/main.gd
+++ prototypesym/main.gd
@@ -1,39 +1,53 @@
 extends Node2D
 
 
+@onready var symmetric_link_res = preload("res://symmetry_link.tscn")
+@onready var cur_side = $above
+
 var character
 
 
 func _ready():
 	character = $character
-	for object in $world_objects.get_children():
+	for object in $above.get_children():
 		var new_object = object.duplicate()
+		var object_symmetric_link = symmetric_link_res.instantiate()
+		object_symmetric_link.obj = new_object
+		object.add_child(object_symmetric_link)
+		var new_object_symmetric_link = symmetric_link_res.instantiate()
+		new_object_symmetric_link.obj = object
+		new_object.add_child(new_object_symmetric_link)
 		#new_object.set_collision_layer_value(1,false)
-		object.add_child(new_object)
+		$below.add_child(new_object)
 		
 func _process(_delta):
 	
 	if Input.is_action_just_pressed("scroll_up"):
-		for object in $world_objects.get_children():
-			object.position.y -= 3
+		for object in cur_side.get_children():
+			object.position.y -= 3 * (1 if cur_side == $above else -1)
 	elif Input.is_action_just_pressed("scroll_down"):
-		for object in $world_objects.get_children():
-			object.position.y += 3
+		for object in cur_side.get_children():
+			object.position.y += 3 * (1 if cur_side == $above else -1)
 	
+	character.check_is_above()
+	if character.is_above:
+		cur_side = $above
+	else:
+		cur_side = $below
 	
-	for object in $world_objects.get_children():
-		#object.global_position += Vector2(1,-1)
-		#object.get_child(2).set_collision_layer_value(1,false)
-		object.get_child(2).get_child(0).material = null
-		object.get_child(2).z_index = -1
-		object.get_child(2).rotation = -object.rotation * 2
-		object.get_child(2).scale = Vector2(-1,-1)
-		object.get_child(2).global_position.x = object.global_position.x
-		object.get_child(2).global_position.y = (250 - object.global_position.y) + 250 
-		#new_object.global_position.y = (250 - object.global_position.y)  + 250
-		if character.is_above:
-			object.get_child(2).get_node("CollisionShape2D").disabled = true
-			object.get_node("CollisionShape2D").disabled = false
+	for object in cur_side.get_children():
+		var symmetric_object = object.get_node("symmetry_link").obj
+		if cur_side == $above:
+			symmetric_object.get_child(0).material = null
+			symmetric_object.z_index = -1
+			if object.get("gravity_scale") and object.gravity_scale != 1:
+				object.gravity_scale = 1
 		else:
-			object.get_child(2).get_node("CollisionShape2D").disabled = false
-			object.get_node("CollisionShape2D").disabled = true
+			if object.get("gravity_scale") and object.gravity_scale != -1:
+				object.gravity_scale = -1
+		symmetric_object.global_position.y = (250 - object.global_position.y) + 250
+		symmetric_object.rotation = -object.rotation
+		symmetric_object.scale = Vector2(-object.scale.x,-object.scale.y)
+		symmetric_object.global_position.x = object.global_position.x
+		symmetric_object.get_node("CollisionShape2D").disabled = true
+		object.get_node("CollisionShape2D").disabled = false
