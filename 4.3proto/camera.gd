31293084bfb34411253fc1a835b74bf544ba40e0
Rink <bystrova1979inbox.ru@gmail.com>
2024-08-21 17:56:04 +0300

    Made some improvements for player: run animation, coyote time and jump buffer. Made the camera

---
+++ 4.3proto/camera.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+
+@export var LERP_SPEED: float = 0.05
+
+var target
+
+
+func _ready():
+	target = get_tree().current_scene.get_node("character")
+
+func _physics_process(delta):
+	if is_instance_valid(target):
+		var target_pos_x
+		target_pos_x = int(lerp(self.global_position.x, target.position.x, LERP_SPEED))
+		global_position = Vector2(target_pos_x, 250)
--- 4.3proto/character.gd
+++ 4.3proto/character.gd
@@ -11,9 +11,6 @@ func _ready():
 	axis = get_tree().current_scene.get_node("axis")
 
 func _process(_delta):
-	
-	
-	
 	# after calling move_and_slide()
 	for i in get_slide_collision_count():
 		var c = get_slide_collision(i)
@@ -26,11 +23,25 @@ func _process(_delta):
 			velocity.y = 1000
 	check_is_above()
 	
-	if Input.is_action_just_pressed('w') and is_on_floor():
+	if Input.is_action_just_pressed('w'):
+		$jump_buffer_timer.start()
+	if is_on_floor():
+		$coyote_timer.start()
+	
+	if not $jump_buffer_timer.is_stopped() and not $coyote_timer.is_stopped():
 		velocity.y = -500
+		$jump_buffer_timer.stop()
+		$coyote_timer.stop()
 	var horz = Input.get_axis('a','d')
 	velocity.x = 100 * horz
 	velocity.x = 300 * horz
+	
+	if velocity.x > 10:
+		for anim in [$idle, $run, $jump]:
+			anim.flip_h = false
+	elif velocity.x < -10:
+		for anim in [$idle, $run, $jump]:
+			anim.flip_h = true
 	
 	animate()
 	
@@ -40,25 +51,23 @@ func check_is_above():
 	is_above = global_position.y < axis.global_position.y - 10
 
 func animate():
-	
 	if velocity == Vector2(0,0):
-		
-		
-		$idle.visible = true
-		$jump.visible = false
-		$idle.play()
-		$jump.stop()
-
+		play_anim($idle)
 	else:
 		if velocity.y != 0:
-			if velocity.x < 0:
-				$jump.scale.x = -0.2
-			elif velocity.x  > 0:
-				$jump.scale.x = 0.2
-			$idle.visible = false
-			$jump.visible = true
-			$jump.play()
-			$idle.stop()
-		
-			
-	
+			play_anim($jump)
+		else:
+			play_anim($run)
+
+func play_anim(what):
+	for anim in [$idle, $run, $jump]:
+		if anim == what:
+			anim.visible = true
+			anim.play()
+			if anim == $run:
+				$RunAnimationPlayer.play("running")
+			else:
+				$RunAnimationPlayer.play("not running")
+		else:
+			anim.visible = false
+			anim.stop()
