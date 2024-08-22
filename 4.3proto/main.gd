050712519bf41e89501a7eaaaeda8d5377309f32
marz640 <marksenz46@gmail.com>
2024-08-22 22:13:55 +0100

    level menu

--- 4.3proto/main.gd
+++ 4.3proto/main.gd
@@ -1,20 +1,23 @@
 extends Node2D
 
-
 @onready var above_material = preload("res://above_material.tres")
 @onready var symmetric_link_res = preload("res://symmetry_link.tscn")
-@onready var cur_side = $above
+@onready var cur_side 
 
-var character
+@onready var character = $character
 
+#replace _ready with create(), so can be called for each level creation
 
-func _ready():
-	character = $character
+func create_level():
+	
+	print('run')
 	for object in $above.get_children():
+		
 		if not(object is RigidBody2D): # Idk, but the push is feeling bad when I lay material on its sprite
 			for child in object.get_children():
 				if child is Sprite2D:
 					child.material = above_material
+					
 		var new_object = object.duplicate()
 		var object_symmetric_link = symmetric_link_res.instantiate()
 		object_symmetric_link.obj = new_object
@@ -24,12 +27,18 @@ func _ready():
 		new_object.add_child(new_object_symmetric_link)
 		#new_object.set_collision_layer_value(1,false)
 		$below.add_child(new_object)
+		
 	if Global.lst_checkpoint_pos:
-		$character.global_position = Global.lst_checkpoint_pos
-	$camera.global_position.x = $character.global_position.x
-	set_symmetric()
+		character.global_position = Global.lst_checkpoint_pos
+	$camera.global_position.x = character.global_position.x
+
+var level_needs_setup = true
 
 func _process(_delta):
+	
+	if len($menu/level_objects/level_1.get_children()) == 0 and level_needs_setup:
+		$".".create_level()
+		level_needs_setup = false
 	# NOT FORGET TO SET NULL IN GLOBAL.LST_CHECKPOINT_POS WHEN YOU EXIT LEVEL OR FINISHING IT UP
 	if Input.is_action_just_pressed("scroll_up"):
 		for object in cur_side.get_children():
@@ -48,28 +57,28 @@ func _process(_delta):
 		cur_side = $below
 	set_symmetric()
 
-
 func set_symmetric():
-	for object in cur_side.get_children():
-		var symmetric_object = object.get_node("symmetry_link").obj
-		if cur_side == $above:
-			for child in symmetric_object.get_children():
-				if child is Sprite2D:
-					child.material = null
-			symmetric_object.z_index = -1
-			#if object.get("gravity_scale") and object.gravity_scale != 1:
-				#object.gravity_scale = 1
-		else:
-			
-			if object.get("gravity_scale") and object.gravity_scale != -1:
-				pass
-				#ProjectSettings.set_setting("physics/2d/default_gravity_vector", Vector2(0, 1))
-		symmetric_object.global_position.y = (250 - object.global_position.y) + 250
-		symmetric_object.rotation = -object.rotation
-		symmetric_object.scale = Vector2(-object.scale.x,-object.scale.y)
-		symmetric_object.global_position.x = object.global_position.x
-		symmetric_object.get_node("CollisionShape2D").disabled = true
-		object.get_node("CollisionShape2D").disabled = false
-		# for some specific parameters of specific objects
-		if object.has_method("set_up_symmetric_object"):
-			object.set_up_symmetric_object()
+	if level_needs_setup == false:
+		for object in cur_side.get_children():
+			var symmetric_object = object.get_node("symmetry_link").obj
+			if cur_side == $above:
+				for child in symmetric_object.get_children():
+					if child is Sprite2D:
+						child.material = null
+				symmetric_object.z_index = -1
+				#if object.get("gravity_scale") and object.gravity_scale != 1:
+					#object.gravity_scale = 1
+			else:
+				
+				if object.get("gravity_scale") and object.gravity_scale != -1:
+					pass
+					#ProjectSettings.set_setting("physics/2d/default_gravity_vector", Vector2(0, 1))
+			symmetric_object.global_position.y = (250 - object.global_position.y) + 250
+			symmetric_object.rotation = -object.rotation
+			symmetric_object.scale = Vector2(-object.scale.x,-object.scale.y)
+			symmetric_object.global_position.x = object.global_position.x
+			symmetric_object.get_node("CollisionShape2D").disabled = true
+			object.get_node("CollisionShape2D").disabled = false
+			# for some specific parameters of specific objects
+			if object.has_method("set_up_symmetric_object"):
+				object.set_up_symmetric_object()
---
+++ 4.3proto/menu.gd
@@ -0,0 +1,52 @@
+extends CanvasLayer
+
+#when a level is beaten, add it to 'unlocked_levels'
+
+var levels = [1,2,3,4,5,6]
+var unlocked_levels = [1]
+
+func _on_level_1_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and Input.is_action_just_pressed("left_click"):
+		start_level(1)
+
+func _on_level_2_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and Input.is_action_just_pressed("left_click"):
+		start_level(2)
+
+func _on_level_3_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and Input.is_action_just_pressed("left_click"):
+		start_level(3)
+
+func _on_level_4_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and Input.is_action_just_pressed("left_click"):
+		start_level(4)
+
+func _on_level_5_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and Input.is_action_just_pressed("left_click"):
+		start_level(5)
+
+func _on_level_6_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and Input.is_action_just_pressed("left_click"):
+		start_level(6)
+
+func start_level(level):
+	
+	# if level is unlocked
+	if level in unlocked_levels:
+	
+		if level == 1:
+			Global.current_level = 1
+			for object in $level_objects/level_1.get_children():
+				object.get_parent().remove_child(object)
+				object.process_mode = Node.PROCESS_MODE_INHERIT
+				object.visible = true
+				$"../above".call_deferred("add_child",object) 
+				#$"..".create_level()
+				#Global.transfer_objects.append(object)
+				
+
+			#$"..".create_level()
+			$"../character".global_position = Vector2(100,-100)
+			
+	
+			$Sprite2D.visible = false
