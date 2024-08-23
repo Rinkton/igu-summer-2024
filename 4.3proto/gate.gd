b1fe8233317ce9741991a41cbabf95a44b87a5e1
Rink <bystrova1979inbox.ru@gmail.com>
2024-08-23 19:44:43 +0300

    Started pressure buttons and gates fix

--- 4.3proto/gate.gd
+++ 4.3proto/gate.gd
@@ -13,8 +13,15 @@ func deactivate():
 	activated = false
 
 func set_up_symmetric_object():
-	get_node("symmetry_link").obj.get_node("Sprite2D").scale = $Sprite2D.scale
-	get_node("symmetry_link").obj.get_node("Sprite2D").position = $Sprite2D.position
-	get_node("symmetry_link").obj.get_node("CollisionShape2D").position = $CollisionShape2D.position
-	get_node("symmetry_link").obj.get_node("CollisionShape2D").scale = $CollisionShape2D.scale
-	get_node("symmetry_link").obj.get_node("CollisionShape2D").shape.size = $CollisionShape2D.shape.size
+	#if get_parent().name == "above":
+		get_node("symmetry_link").obj.get_node("Sprite2D").scale = $Sprite2D.scale
+		get_node("symmetry_link").obj.get_node("Sprite2D").position = $Sprite2D.position
+		get_node("symmetry_link").obj.get_node("CollisionShape2D").position = $CollisionShape2D.position
+		get_node("symmetry_link").obj.get_node("CollisionShape2D").scale = $CollisionShape2D.scale
+		get_node("symmetry_link").obj.get_node("CollisionShape2D").shape.size = $CollisionShape2D.shape.size
+	#else:
+	#	$Sprite2D.scale
+	#	$Sprite2D.position
+	#	$CollisionShape2D.position
+	#	$CollisionShape2D.scale
+	#	$CollisionShape2D.shape.size
--- 4.3proto/pressure_button.gd
+++ 4.3proto/pressure_button.gd
@@ -5,20 +5,29 @@ extends Area2D
 
 
 func _physics_process(_delta):
-	if len(get_overlapping_bodies()) > 0:
-		for body in get_overlapping_bodies():
-			if body is StaticBody2D:
-				continue
-			if not get_node(node_path).activated:
-				get_node(node_path).activate()
-				$AnimationPlayer.play("press")
-	else:
-		if get_node(node_path).activated:
-			get_node(node_path).deactivate()
-			$AnimationPlayer.play_backwards("press")
+	var overlapping_bodies = get_overlapping_bodies()
+	var i = 0
+	while i < len(overlapping_bodies):
+		if overlapping_bodies[i] is StaticBody2D:
+			overlapping_bodies.remove_at(i)
+		else:
+			i+=1
+	if $Timer.is_stopped():
+		if len(overlapping_bodies) > 0:
+			for body in get_overlapping_bodies():
+				if not get_node(node_path).activated:
+					get_node(node_path).activate()
+					$AnimationPlayer.play("press")
+					$Timer.start()
+		else:
+			if get_node(node_path).activated:
+				get_node(node_path).deactivate()
+				$AnimationPlayer.play_backwards("press")
+				$Timer.start()
 
 
 func set_up_symmetric_object():
+	pass
 	get_node("symmetry_link").obj.get_node("Sprite2D").scale = $Sprite2D.scale
 	get_node("symmetry_link").obj.get_node("Sprite2D").position = $Sprite2D.position
 
