781498103c2a13cb2f5bdf1660dc77003b3a75a4
Rink <bystrova1979inbox.ru@gmail.com>
2024-08-24 22:33:51 +0300

    Fixed some mistakes in level 2

--- 4.3proto/main.gd
+++ 4.3proto/main.gd
@@ -20,7 +20,6 @@ func _ready(): ################################
 
 func create_level():
 	
-	print('run')
 	for object in $above.get_children():
 		
 		if not(object is RigidBody2D): # Idk, but the push is feeling bad when I lay material on its sprite
@@ -54,9 +53,9 @@ func _physics_process(delta):
 	"""
 	if can_scroll:
 		if Input.is_action_pressed("scroll_down") or Input.is_action_pressed("scroll_up"): # arrows
-			scroll_max_spd = 135
-			scroll_acc = 6000
-			scroll_dec = 2000
+			scroll_max_spd = 115
+			scroll_acc = 4000
+			scroll_dec = 6000
 		else:
 			scroll_max_spd = 180
 			scroll_acc = 4000
