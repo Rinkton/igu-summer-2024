855ae6bdb030c7a5e53003368d2f6d9c3b7914ee
Rink <bystrova1979inbox.ru@gmail.com>
2024-08-25 03:32:08 +0300

    merged

--- 4.3proto/levels/scroll_trigger.gd
+++
@@ -1,7 +0,0 @@
-extends Marker2D
-
-
-func _process(delta):
-	if get_tree().current_scene.get_node("character").global_position.x > global_position.x:
-		get_tree().current_scene.can_scroll = true
-		queue_free()
--- 4.3proto/scroll_trigger.gd
+++ 4.3proto/scroll_trigger.gd
@@ -4,5 +4,4 @@ extends Marker2D
 func _process(delta):
 	if get_tree().current_scene.get_node("character").global_position.x > global_position.x:
 		get_tree().current_scene.can_scroll = true
-		print("CAN SCROLL")
 		queue_free()
